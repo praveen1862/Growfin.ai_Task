21c6ebd3ac5f172d9ee84580b4b616047f0220e7
Praveen S <121714857+praveen1862@users.noreply.github.com>
2024-09-10 23:33:31 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.querySelector('.demo-button').addEventListener('click', () => {
+    alert('Schedule a Demo clicked!');
+    // Add your logic here
+});
+
+document.querySelector('.access-button').addEventListener('click', () => {
+    alert('Request Access clicked!');
+    // Add your logic here
+});
